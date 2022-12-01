5d85360d555770fdf975b3dcffedf9c6f077fbee
Velma Kimani <stanvelma16@gmail.comm>
2022-12-01 15:34:13 +0300

    Add firebase components

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -5,9 +5,40 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import 'package:foodapp/app/app.dart';
-import 'package:foodapp/bootstrap.dart';
+import 'dart:async';
+import 'dart:developer';
 
-void main() {
-  bootstrap(() => const App());
+import 'package:device_preview/src/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:foodapp/bootstrap.dart';
+import 'package:foodapp/screens/device_setup.dart';
+
+import 'package:foodapp/utils/_index.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  FoodAppConfig(
+    values: FoodAppValues(
+      baseDomain: 'https://api.spoonacular.com/recipes',
+      hiveBox: 'foodapp_development',
+    ),
+  );
+
+  FlutterError.onError = (details) {
+    log(details.exceptionAsString(), stackTrace: details.stack);
+  };
+
+  await bootstrap(
+    () => DevicePreview(
+      enabled: false,
+      builder: (context) => MultiBlocProvider(
+        providers: Singletons.registerCubits(),
+        child: const DeviceSetUp(),
+      ),
+    ),
+  );
 }
