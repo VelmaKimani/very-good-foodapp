b1a222dfcedaada01c54639ff8c77984704c264f
Velma Kimani <stanvelma16@gmail.comm>
2022-12-01 15:39:11 +0300

    Add application theme colors

---
+++ lib/utils/theme.dart
@@ -0,0 +1,10 @@
+part of foodapp_utils;
+
+const kPrimaryColor = Color(0xFF26C6DA);
+const kPrimaryBackgroundColor = Colors.white;
+const kIndicatorColor = Color.fromRGBO(35, 31, 31, .2);
+const kVerseContainerColor = Color(0xfffffbf9);
+const kVerseContainerBorderColor = Color(0xfffff2eb);
+const kCommentBackgroundColor = Color(0xfff5f5f6);
+const kCommentBorderColor = Color(0xffdadada);
+const kDeletePostColor = Color(0xFFE57373);
